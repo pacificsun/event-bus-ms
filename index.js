4dc8fcfc3cf3284ae18ba5161ea565bda9ff3c25
pacificsuraj <stsurajst@gmail.com>
2021-05-05 08:26:12 +0545

    query service sync

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const app = express();
 
 app.use(express.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
   console.log(req.body);
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
@@ -17,6 +21,10 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4003/events", event);
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => console.log("Listening on 4005"));
